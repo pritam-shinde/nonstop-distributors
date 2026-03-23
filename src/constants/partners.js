beb8f03470686373da9b5d7eab7b4d65c7ef4cb1
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2026-03-23 18:00:13 +0530

    updated content for partners page

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -2,51 +2,49 @@ export const partners = [
   {
     name: "Patanjali",
     logoSrc: "/assets/distributor-img/partners/patanjali.png",
-    // logoSrc: "/assets/distributor-img/pantanjali.png",
     width: 214,
     height: 60,
     category: "Ghee, Oils & Staples",
     subcategory: "Food, Oil and Biscuits",
     coverSrc: "/assets/distributor-img/partners/patanjali.jpg",
-    description: "Founded in 2006, Patanjali deals in Ayurvedic products, food items, and healthcare solutions. It has become one of the largest brands in the FMCG sector in India, promoting swadeshi products. Supporting its growth through strong retail distribution and reliable supply networks.",
+    description:
+      "Founded in 2006, Patanjali deals in Ayurvedic products, food items, and healthcare solutions. It has become one of the largest brands in the FMCG sector in India, promoting swadeshi products.  The brand offers a wide range of food products, edible oils, and biscuits across Indian households.",
   },
   {
     name: "Britannia",
     logoSrc: "/assets/distributor-img/partners/britania.png",
-    // logoSrc: "/assets/distributor-img/britannia.png",
     width: 168,
     height: 60,
     category: "Foods & Beverages",
     subcategory: "Biscuits & Snacks",
     coverSrc: "/assets/distributor-img/partners/britania.jpg",
     description:
-      "Established in 1892, Britannia deals in biscuits, dairy products, cakes, and breads. It is one of India’s most trusted food brands with a strong legacy and market leadership in the biscuit segment. Ensuring efficient FMCG distribution across supermarkets, retail stores, and wholesale markets.",
+      "Established in 1892, Britannia deals in biscuits, dairy products, cakes, and breads. It is one of India’s most trusted food brands with a strong legacy and market leadership in the biscuit segment. Its popular products include Good Day, Marie Gold, Bourbon, Little Hearts, and NutriChoice.",
   },
   {
     name: "Hershey's",
     logoSrc: "/assets/distributor-img/partners/hersley.png",
-    // logoSrc: "/assets/distributor-img/hersheys.jpg",
     width: 150,
     height: 60,
     category: "Confectionery",
     subcategory: "Chocolates",
     coverSrc: "/assets/distributor-img/partners/hersheys.jpg",
     description:
-      "Founded in 1894, Hershey’s deals in chocolates, confectionery, and snacks, and is a globally recognized brand known for iconic products like Hershey’s Kisses and chocolate bars. Delivering reliable supply with a focus on freshness, timely delivery, and wide market reach.",
+      "Founded in 1894, Hershey’s deals in chocolates, confectionery, and snacks, and is a globally recognized brand known for iconic products like Hershey’s Kisses and chocolate bars. The portfolio includes chocolate bars, syrups, spreads, and flavored milk products.",
   },
   {
     name: "Keya",
     logoSrc: "/assets/distributor-img/partners/keya.png",
-    // logoSrc: "/assets/distributor-img/keya-logo.jpg",
     width: 112,
     height: 60,
     category: "Spices & Seasonings",
     subcategory: "Spices & herbs",
     coverSrc: "/assets/distributor-img/partners/keya.jpg",
     description:
-      "Established in 2009, Keya deals in premium herbs, spices, seasonings, and condiments. It is known for introducing international-quality seasonings to Indian kitchens. Ensuring efficient distribution of household essentials through structured channels and strong inventory management.",
+      "Established in 2009, Keya deals in premium herbs, spices, seasonings, and condiments. It is known for introducing international-quality seasonings to Indian kitchens. The brand is popular for oregano, chilli flakes, mixed herbs, pasta seasoning, and spice grinders.",
   },
 ];
+
 // export const partners = [
 //   {
 //     name: "Patanjali",
